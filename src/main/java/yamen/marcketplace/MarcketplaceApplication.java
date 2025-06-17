d95edab4344e2ed68d2531fdfc077b5d2bd0ed26
YamenAmjed22 <yamenamjed22@gmail.com>
2025-06-17 00:09:00 -0900

    i make secret for my DB Conecction

--- src/main/java/yamen/marcketplace/MarcketplaceApplication.java
+++ src/main/java/yamen/marcketplace/MarcketplaceApplication.java
@@ -2,11 +2,23 @@ package yamen.marcketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MarcketplaceApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load(); // Loads .env file
+
+		// Set environment variables manually
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+
 		SpringApplication.run(MarcketplaceApplication.class, args);
 	}
 
