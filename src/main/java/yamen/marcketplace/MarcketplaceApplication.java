0c71a08d8d63c1f5f550f888aabaa3d7503f65a8
Yamen Amjed <yamenamjed2022@gmail.com>
2026-02-13 06:55:36 -0800

    make exchange on put proparity

--- src/main/java/yamen/marcketplace/MarcketplaceApplication.java
+++ src/main/java/yamen/marcketplace/MarcketplaceApplication.java
@@ -9,15 +9,6 @@ public class MarcketplaceApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load(); // Loads .env file
-
-		// Set environment variables manually
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
-		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 
 		SpringApplication.run(MarcketplaceApplication.class, args);
 	}
